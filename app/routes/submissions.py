3933a973feef51058bf4e0f49e8d42129416db57
Akshat <ojhaakshat429@gmail.com>
2026-03-09 23:55:43 +0530

    feat/new submission route

---
+++ app/routes/submissions.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, UploadFile, File, Form
+from app.db.supabase_client import supabase
+import uuid
+
+router = APIRouter()
+
+
+@router.post("/submit-answer")
+async def submit_answer(
+    exam_id: str = Form(...), student_id: str = Form(...), file: UploadFile = File(...)
+):
+
+    submission_id = str(uuid.uuid4())
+
+    file_path = f"{submission_id}_{file.filename}"
+
+    content = await file.read()
+
+    # upload to supabase storage
+    supabase.storage.from_("answer-sheets").upload(file_path, content)
+
+    # insert into database
+    supabase.table("submissions").insert(
+        {
+            "id": submission_id,
+            "exam_id": exam_id,
+            "student_id": student_id,
+            "pdf_path": file_path,
+        }
+    ).execute()
+
+    return {"message": "upload successful", "submission_id": submission_id}
