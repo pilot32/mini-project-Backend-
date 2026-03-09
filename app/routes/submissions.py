9994828430987054beeb9cb28c5d8fec2709300f
Akshat <ojhaakshat429@gmail.com>
2026-03-10 01:07:28 +0530

    fix: added the validation for file type and file sizes

--- app/routes/submissions.py
+++ app/routes/submissions.py
@@ -1,15 +1,25 @@
-from fastapi import APIRouter, UploadFile, File, Form
+from fastapi import APIRouter, UploadFile, File, Form, HTTPException
 from app.db.supabase_client import supabase
 import uuid
 
 router = APIRouter()
+
+MAX_SIZE = 10 * 1024 * 1024
 
 
 @router.post("/submit-answer")
 def submit_answer(
     exam_id: str = Form(...), student_id: str = Form(...), file: UploadFile = File(...)
 ):
+    # validatng the file type
 
+    if file.content_type != "application/pdf" or not file.filename.lower().endswith(
+        ".pdf"
+    ):
+        raise HTTPException(status_code=400, details="Only PDFs are allowed")
+    content = file.file.read()
+    if len(content) > MAX_SIZE:
+        raise HTTPException(status=400, detial="upload lower size file")
     submission_id = str(uuid.uuid4())
 
     file_path = f"{submission_id}_{file.filename}"
